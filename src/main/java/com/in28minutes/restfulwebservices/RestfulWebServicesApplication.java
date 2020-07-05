b6bf407c0c8db4992254d64dab0763c1aed8c9a5
shadman <shadman@shadman>
2020-07-05 17:38:59 +0530

    Package renamed

--- src/main/java/com/in28minutes/restfulwebservices/RestfulWebServicesApplication.java
+++ src/main/java/com/in28minutes/restfulwebservices/RestfulWebServicesApplication.java
@@ -1,4 +1,4 @@
-package com.in28minutes.rest.webservices.restfulwebservices;
+package com.in28minutes.restfulwebservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
